f03d4f4ca26f3f5aec5f1227b3a8bd5b432eaeb3
Eduardo <edudavid2005@hotmail.com>
2026-03-26 12:21:59 -0500

    Creacion de la clase Libro

---
+++ Libro.java
@@ -0,0 +1,26 @@
+package Java.ejercicios.Ejercicio3;
+
+public class Libro {
+
+    private final String titulo;
+    private final String autor;
+    private final int anio;
+    private boolean disponible;
+
+
+    public Libro(String titulo, String autor, int anio, boolean disponible)
+    {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+        this.disponible = disponible;
+    }
+
+    public void mostrarInfo()
+    {
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Año: " + anio);
+        System.out.println("Disponible: " + disponible);
+    }
+}
